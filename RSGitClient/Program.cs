8666bd8190dd96169f9f08857758727ea1931859
andyhebear <andyhebear@163.com>
2024-06-04 15:38:15 +0800

    add git client
    add git client

---
+++ RSGitClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RSGit;
+
+namespace RSGitClient
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //args = new string[] { "daemon", "49750" };
+            var output = new CommandLineHandling().Handle(new MiniGit(new DirectoryInfo(".").FullName), CommandLineHandling.Config, args);
+            Console.WriteLine(output);
+        }
+    }
+}
